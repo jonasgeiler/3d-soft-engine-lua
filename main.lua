e3af015cb0f295f479d13ee29d2ebe3d98888265
Jonas Geiler <git@jonasgeiler.com>
2024-06-04 19:53:43 +0200

    feat: made the demo more interactive with arrow keys

--- main.lua
+++ main.lua
@@ -4,21 +4,24 @@ local Camera = require('lib.camera')
 local Device = require('lib.device')
 local Vector3 = require('lib.vector3')
 
-local window = fenster.open(640, 480, '3D Soft Engine')
+local window = fenster.open(640, 480, '3D Soft Engine - Rotate with arrow keys')
 
 local camera = Camera.new(Vector3.new(0, 0, 10), Vector3.new(0, 0, 0))
 local device = Device.new(window)
 
 local meshes = Device.load_json_file('./assets/monkey.babylon')
+--local meshes = Device.load_json_file('./assets/teapot.babylon')
+--local meshes = Device.load_json_file('./assets/torus.babylon')
 
--- Set the rotation of the meshes to 180 degrees
+-- Set the initial rotation/position of the meshes
 for mi = 1, #meshes do
 	local mesh = meshes[mi]
-	mesh.rotation.y = 3.14159 -- 180 degrees
+	mesh.rotation.y = 3.14159 -- rotate 180 degrees to see the monkey's face
 end
 
 while device:present() do
 	local delta_time = device.window.delta
+	local keys = device.window.keys
 
 	device:clear()
 
@@ -34,8 +37,22 @@ while device:present() do
 
 	for mi = 1, #meshes do
 		local mesh = meshes[mi]
-		mesh.rotation.y = mesh.rotation.y + 0.5 * delta_time
+
 		mesh.rotation.x = mesh.rotation.x + 0.5 * delta_time
+		mesh.rotation.y = mesh.rotation.y + 0.5 * delta_time
+
+		if keys[17] then -- up arrow
+			mesh.rotation.x = mesh.rotation.x + 1 * delta_time
+		end
+		if keys[18] then -- down arrow
+			mesh.rotation.x = mesh.rotation.x - 1 * delta_time
+		end
+		if keys[19] then -- right arrow
+			mesh.rotation.y = mesh.rotation.y - 1 * delta_time
+		end
+		if keys[20] then -- left arrow
+			mesh.rotation.y = mesh.rotation.y + 1 * delta_time
+		end
 	end
 
 	device:render(camera, meshes)
